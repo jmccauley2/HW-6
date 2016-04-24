f80c15fe1132a0cd06512c6d8ea013e609acbd6d
jmccauley2 <jmccauley2@luc.edu>
2016-04-24 16:23:31 -0500

    Added files via upload
    Adds functionality for extraction method and quadratic probing w/o replacement.  Creates ofstream out.

--- HW6.cpp
+++ HW6.cpp
@@ -8,10 +8,21 @@
 
 using namespace std;
 
-
+int extraction_method(int key);
+bool quadratic_probing_without_repacement(int key,int*hash_table,int M, int i);
 
 int main()
 {
+	int input=0;
+	while (input < 1 || input>450000000)
+	{
+		cout << "enter a number between 1 and 450 million" << endl;
+		cin >> input;
+		if (input < 1 || input>450000000)
+			cout << "invalid input" << endl;
+	}
+
+
 	ifstream in;
 	int SSnum[65000];
 	int SShash[65000] = { 0 };
@@ -23,7 +34,7 @@ int main()
 	}
 	char num[9];
 	int j = 0;
-	while (in.good())
+	while (in.good())//stores SS nums in array
 	{
 		in.getline(num,10,',');
 
@@ -31,6 +42,46 @@ int main()
 		j++;
 	}
 
+	for (j = 0; j < 65000; j++)//hashing nums to table
+	{
+		int address = extraction_method(SSnum[j]);
+		if (SShash[address] == 0)
+			SShash[address] = SSnum[j];
+		else
+			quadratic_probing_without_repacement(SSnum[j], SShash, 65000, input);
+	}
+	ofstream out;
+	out.open("hashed_socials.txt");
 	system("pause");
 	return 0;
+}
+
+int extraction_method(int key)
+{
+	int digit3 = (key / 1000000) % 10;
+	int digit5 = (key / 1000) % 10;
+	int digit7 = (key / 100) % 10;
+	int digit8 = (key / 10) % 10;
+	return (digit3 * 1000) + (digit5 * 100) + (digit7 * 10) + digit8;
+}
+
+bool quadratic_probing_without_repacement(int key, int*hash_table, int M, int i)//MAY NEED TO CHANGE
+{
+	int address = (extraction_method(key) + i*i)%M;
+	int j;
+	for (j = 0; j < 65000; j++ )
+	{
+		if (hash_table[address] == 0)
+		{
+			hash_table[address] = key;
+			return true;
+		}
+		else
+		{
+			(address++)%M;	
+		}
+	}
+	if (j >= 65000)//all addresses were checked
+		return false;
+	
 }
